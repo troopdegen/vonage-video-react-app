a2bd274651f8a6b2ac8fff9e39f63b9fe935f019
Misha Behei <67476711+behei-vonage@users.noreply.github.com>
2025-02-07 09:53:36 -0600

    VIDCS-3303: Improve .env config documentation and example (#33)
    Co-authored-by: dwivedisachin <sachin.dwivedi@vonage.com>

--- backend/services/tests/feedbackService.test.ts
+++ backend/services/tests/feedbackService.test.ts
@@ -1,13 +1,22 @@
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it, beforeAll, afterAll, jest } from '@jest/globals';
 import { FeedbackService } from '../feedbackService';
-import JiraFeedbackService from '../jiraFeedbackService';
 
 describe('getFeedbackService', () => {
+  const originalEnv = process.env;
+  beforeAll(() => {
+    jest.resetModules();
+    process.env = { ...originalEnv, VIDEO_SERVICE_PROVIDER: 'opentok' };
+  });
+
+  afterAll(() => {
+    process.env = originalEnv;
+  });
+
   it('should return JiraFeedbackService when asking for a service', async () => {
     const { default: getFeedbackService } = await import('../getFeedbackService');
 
     const feedbackService: FeedbackService = getFeedbackService();
 
-    expect(feedbackService).toBeInstanceOf(JiraFeedbackService);
+    expect(feedbackService.constructor.name).toBe('JiraFeedbackService');
   });
 });
